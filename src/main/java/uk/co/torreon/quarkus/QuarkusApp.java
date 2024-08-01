b9d688e848f469fcc1e0f2b8b7638cfb02db75e2
susan-laptop <scoulter@torreon.co.uk>
2024-08-01 17:38:05 +0100

    initial commit

---
+++ src/main/java/uk/co/torreon/quarkus/QuarkusApp.java
@@ -0,0 +1,13 @@
+package uk.co.torreon.quarkus;
+
+import io.quarkus.runtime.QuarkusApplication;
+import io.quarkus.runtime.annotations.QuarkusMain;
+
+@QuarkusMain
+public class QuarkusApp implements QuarkusApplication {
+
+    @Override
+    public int run(String... args) throws Exception {
+        return 0;
+    }
+}
